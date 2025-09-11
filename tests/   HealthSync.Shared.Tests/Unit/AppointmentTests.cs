5aa41be33bd5f0d5fcacfbbfcba51b723afd8f95
Adrian Ratajczak <106772824+Adrianowsski@users.noreply.github.com>
2025-09-11 19:41:25 +0200

    Create AppointmentTests.cs

---
+++ tests/   HealthSync.Shared.Tests/Unit/AppointmentTests.cs
@@ -0,0 +1,34 @@
+using System;
+using FluentAssertions;
+using HealthSync.Shared.Models;
+using Xunit;
+
+namespace HealthSync.Shared.Tests.Unit;
+
+public class AppointmentTests
+{
+    [Fact]
+    public void Defaults_should_set_status_scheduled_and_empty_prescriptions()
+    {
+        var a = new Appointment();
+
+        a.Status.Should().Be("scheduled");
+        a.Prescriptions.Should().NotBeNull();
+        a.Prescriptions.Should().BeEmpty();
+    }
+
+    [Fact]
+    public void DisplayName_should_combine_patient_name_and_date()
+    {
+        var a = new Appointment
+        {
+            AppointmentDate = new DateTime(2025, 1, 20, 14, 30, 0),
+            PatientProfile = new PatientProfile { FirstName = "Anna", LastName = "Kowalska" }
+        };
+
+        var expectedDate = a.AppointmentDate.ToString("g"); // ten sam format co w modelu
+        a.DisplayName.Should().Contain("Anna")
+                      .And.Contain("Kowalska")
+                      .And.EndWith(expectedDate);
+    }
+}
