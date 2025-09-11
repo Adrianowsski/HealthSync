c57e4da88d7b51012762f740c4178d745fdc6c4e
Adrian Ratajczak <106772824+Adrianowsski@users.noreply.github.com>
2025-09-11 18:45:16 +0200

    Delete Models directory

--- Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace HealthSync.Intranet.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
