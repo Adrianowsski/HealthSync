2a00e00e144cb9fe80ec908ded4cf17c4efe9a7c
Adrian Ratajczak <106772824+Adrianowsski@users.noreply.github.com>
2025-09-13 12:30:49 +0200

    Update LoginViewModel.cs

--- src/HealthSync.Portal/Models/LoginViewModel.cs
+++ src/HealthSync.Portal/Models/LoginViewModel.cs
@@ -1,14 +1,15 @@
-﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations;
 
-namespace HealthSync.Portal.Models;
-
-public class LoginViewModel
+namespace HealthSync.Portal.Models
 {
-    [Required]
-    [EmailAddress]
-    public string Email { get; set; }
+    public class LoginViewModel
+    {
+        [Required, EmailAddress]
+        public string Email { get; set; } = string.Empty;
 
-    [Required]
-    [DataType(DataType.Password)]
-    public string Password { get; set; }
+        [Required, DataType(DataType.Password)]
+        public string Password { get; set; } = string.Empty;
+
+        public bool RememberMe { get; set; }
+    }
 }
